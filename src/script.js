93ebe812ae1268ea9e05640ca873487601fe658f
Ludwig <41292435+TheNarh@users.noreply.github.com>
2025-07-02 17:01:19 +0000

    Implemented first part of game logic

--- src/script.js
+++ src/script.js
@@ -1,3 +1,13 @@
 'use strict';
 
-document.querySelector('.message')
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = document.querySelector('.guess').value;
+
+    console.log(guess, typeof guess);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No number!';
+    }
+
+    
+})
