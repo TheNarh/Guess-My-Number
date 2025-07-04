5ded233a4c5c56fa849a4443fe1ed5400863b0ed
Ludwig <41292435+TheNarh@users.noreply.github.com>
2025-07-04 11:16:26 +0000

    Implemented a bit more game logic.

--- src/script.js
+++ src/script.js
@@ -1,13 +1,33 @@
 'use strict';
 
-document.querySelector('.check').addEventListener('click', function() {
-    const guess = document.querySelector('.guess').value;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
-    console.log(guess, typeof guess);
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
 
     if (!guess) {
-        document.querySelector('.message').textContent = 'No number!';
+        document.querySelector('.message').textContent = '❌ No Number!';
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '✅ Correct Number!';
+    } else if (guess > secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = '📈 Too high';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = '📛You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        } 
+    } else if (guess < secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = '📉 Too low';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = '📛You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
     }
-
-    
 })
